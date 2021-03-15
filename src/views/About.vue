a9c27bb77e32ae23bb2a039a3841a38cbb2f00f2
Michael Santini <michael.santinimc@gmail.com>
2021-03-15 15:54:02 +0100

    test2

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,5 @@
 <template>
   <div class="about">
-<<<<<<< HEAD
-    <p>test</p>
-    <p>Cacahuete</p>
-=======
     <p>test2</p>
->>>>>>> 188463e9b1241cb1b0f1f85ac3aa989fa89f9ca6
   </div>
 </template>
