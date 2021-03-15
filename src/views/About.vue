bfdbfe10d8dec43e71d4050d855c9f3d2ecb2e35
chevillon loic <chevillonloic@gmail.com>
2021-03-15 15:55:23 +0100

    ajout salut micha

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="about">
-<<<<<<< HEAD
     <p>test</p>
     <p>Cacahuete</p>
-=======
     <p>test2</p>
->>>>>>> 188463e9b1241cb1b0f1f85ac3aa989fa89f9ca6
+    <p>salut micha</p>
   </div>
 </template>
