daa25cddbec64fdc0f287e878606e7a9bb094fff
chevillon loic <chevillonloic@gmail.com>
2021-03-15 21:08:50 +0100

    debut page inscription

---
+++ src/views/Subscribe.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="subscribe">
+    <h1>Inscription</h1>
+    <label for="email">E-mail</label>
+    <input type="text" name="email" />
+    <label for="name">Prénom</label>
+    <input type="text" name="name" />
+    <label for="surname">Nom de famille</label>
+    <input type="text" name="surname" />
+    <label for="password">Mot de passe</label>
+    <input type="text" name="password" />
+    <label for="repeatpassword">Répéter le mot de passe</label>
+    <input type="text" name="repeatpassword" />
+    <label for=""></label>
+    <select name="" id="">
+      <option value="">Junior</option>
+      <option value=""></option>
+      <option value=""></option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#subscribe {
+  background-color: rgb(228, 222, 222);
+  width: 40%;
+  height: 50vh;
+  margin: auto;
+}
+</style>
