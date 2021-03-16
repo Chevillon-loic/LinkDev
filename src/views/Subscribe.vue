9a8c5618fced4e741d6221281fa87a51839c7bc5
chevillon loic <chevillonloic@gmail.com>
2021-03-16 12:20:40 +0100

    ajout navbar inscription & connection

--- src/views/Subscribe.vue
+++ src/views/Subscribe.vue
@@ -1,72 +1,92 @@
 <template>
-  <form id="subscribe">
-    <h1>Inscription</h1>
-    <div id="nameSurname">
-      <div>
-        <label for="name">Prénom</label>
-        <input type="text" name="name" v-model="name" />
+  <div>
+    <NavBar />
+    <form id="subscribe">
+      <h1>Inscription</h1>
+      <div id="pseudo">
+        <label for="pseudo">Pseudo</label>
+        <input type="text" name="pseudo" v-model="user.pseudo" />
+      </div>
+      <div id="divEmail">
+        <label for="email">E-mail</label>
+        <input type="text" name="email" v-model="user.email" />
       </div>
       <div>
-        <label for="surname">Nom de famille</label>
-        <input type="text" name="surname" v-model="surname" />
+        <label for="password">Mot de passe</label>
+        <input type="text" name="password" v-model="user.password" />
       </div>
-    </div>
-    <div id="divEmail">
-      <label for="email">E-mail</label>
-      <input type="text" name="email" v-model="email" />
-    </div>
-    <div>
-      <label for="password">Mot de passe</label>
-      <input type="text" name="password" v-model="password" />
-    </div>
-    <div>
-      <label for="repeatPassword">Répéter le mot de passe</label>
-      <input type="text" name="repeatPassword" />
-    </div>
-    <!-- <div>
+      <div>
+        <label for="repeatPassword">Répéter le mot de passe</label>
+        <input
+          type="text"
+          name="repeatPassword"
+          v-model="user.repeatPassword"
+        />
+      </div>
+      <!-- <div>
       <span>Genre</span><br />
       <label for="Homme">Homme</label>
       <input type="checkbox" name="Homme" />
       <label for="Femme">Femme</label>
       <input type="checkbox" name="Femme" />
     </div> -->
-    <!-- <div>
+      <!-- <div>
       <span>Année de naissance</span>
       <input type="date" />
     </div> -->
-    <div>
-      <label for="selectLevel">Niveau de dev</label>
-      <select name="selectLevel" id="selectLevel">
-        <option value="Junior">Junior</option>
-        <option value="Confirmé">Confirmé</option>
-        <option value="Expert">Expert</option>
-      </select>
-    </div>
-    <div>
-      <label for="selectLanguage">langage maitrisé</label>
-      <select name="selectLanguage" id="selectLanguage">
-        <option value="">Javascript</option>
-        <option value="">PHP</option>
-        <option value="">Ruby</option>
-      </select>
-    </div>
-    <button>S'inscrire</button>
-  </form>
+      <div>
+        <label for="selectLevel">Niveau de dev</label>
+        <select name="selectLevel" id="selectLevel" v-model="user.selectLevel">
+          <option value="Junior">Junior</option>
+          <option value="Confirmé">Confirmé</option>
+          <option value="Expert">Expert</option>
+        </select>
+      </div>
+      <div>
+        <label for="selectLanguage">langage maitrisé</label>
+        <select
+          name="selectLanguage"
+          id="selectLanguage"
+          v-model="user.selectLanguage"
+        >
+          <option value="Javascript">Javascript</option>
+          <option value="PHP">PHP</option>
+          <option value="Ruby">Ruby</option>
+        </select>
+      </div>
+      <button @click.prevent="pushUser">S'inscrire</button>
+    </form>
+  </div>
 </template>
 
 <script>
+import NavBar from "../components/NavBar";
 export default {
-  data: () => ({
-    name: "",
-    surname: "",
-    email: "",
-    password: "",
-    selectLevel: "",
-    selectLangage: "",
-  }),
-  // methods: {
-  //   pushUser,
-  // },
+  components: { NavBar },
+  data() {
+    return {
+      users: [],
+      user: {
+        pseudo: "",
+        email: "",
+        password: "",
+        repeatPassword: "",
+        selectLevel: "",
+        selectLanguage: "",
+      },
+    };
+  },
+  methods: {
+    pushUser: function() {
+      if (this.user.password == this.user.repeatPassword) {
+        this.users.push(this.user);
+      } else {
+        alert("echec mdp");
+      }
+      console.log(this.user);
+      console.log(this.users);
+    },
+  },
 };
 </script>
 
